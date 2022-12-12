0a2a8a286c79665312ab0e2d609dee920bdb2142
iamjamesb <iamjamesb@outlook.com>
2022-12-12 12:16:52 -0700

    initial commit

---
+++ build/vehicles/CMakeFiles/vehicles.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../deploy/share/ngrest/services/vehicles.pdb"
+  "../deploy/share/ngrest/services/vehicles.so"
+  "CMakeFiles/vehicles.dir/codegen-ngrest-db/VehiclesEntities.cpp.o"
+  "CMakeFiles/vehicles.dir/codegen/ServiceGroupImpl.cpp.o"
+  "CMakeFiles/vehicles.dir/codegen/VehiclesWrapper.cpp.o"
+  "CMakeFiles/vehicles.dir/src/Vehicles.cpp.o"
+  "codegen-ngrest-db/VehiclesEntities.cpp"
+  "codegen-ngrest-db/VehiclesEntities.h"
+  "codegen/ServiceGroupImpl.cpp"
+  "codegen/ServiceGroupImpl.h"
+  "codegen/VehiclesWrapper.cpp"
+  "codegen/VehiclesWrapper.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vehicles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
